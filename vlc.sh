2392c3a89d1201d6fd94610ae647357e6f7924e0
Lukas Senger <lukas@fridolin.com>
2014-10-08 23:38:50 +0200

    add mplayer fallback and retab

--- vlc.sh
+++ vlc.sh
@@ -2,9 +2,9 @@
 
 for pid in $(pidof plugin-container)
 do
-    video=$(find /proc/$pid/fd/ -lname /tmp/Flash*)
-    if [[ -n "$video" ]]
-    then
-        vlc --quiet --verbose 0 --play-and-exit --qt-minimal-view $video &
-    fi
+	video=$(find /proc/$pid/fd/ -lname /tmp/Flash*)
+	if [[ -n "$video" ]]
+	then
+		(vlc --quiet --verbose 0 --play-and-exit --qt-minimal-view $video || mplayer $video)&
+	fi
 done
