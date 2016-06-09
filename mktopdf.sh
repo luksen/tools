1e3882083fba3b40513cf04095aea67968a9f9bc
Lukas Senger <lukas@fridolin.com>
2016-06-09 17:54:51 +0200

    change mktopdf to pandoc

--- mktopdf.sh
+++ mktopdf.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-tmpfile=mktopdftmp.html
-
-[ $# -eq 2 ] || exit 1
-
-style="$(dirname $0)/data/style.css"
-[ -f style.css ] && style=style.css
-
-markdown $1 | recode -d utf8..html > $tmpfile 
-wkhtmltopdf --user-style-sheet $style $tmpfile $2
-rm $tmpfile
+pandoc -f markdown -t latex -o ${1%.*}.pdf $1
