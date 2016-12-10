f461634470879185c2beb796a7ec30a73c5cbd54
Lukas Senger <lukas@fridolin.com>
2016-12-10 17:48:39 +0100

    vlc: use mplayer

--- vlc.sh
+++ vlc.sh
@@ -5,6 +5,6 @@ do
 	video=$(find /proc/$pid/fd/ -lname /tmp/Flash*)
 	if [[ -n "$video" ]]
 	then
-		(vlc --quiet --verbose 0 --play-and-exit --qt-minimal-view $video || mplayer $video)&
+		(mplayer --quiet --verbose 0 --play-and-exit --qt-minimal-view $video || mplayer $video)&
 	fi
 done
